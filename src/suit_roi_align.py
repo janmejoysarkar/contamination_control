fefeecd67eec916bd02cb3db9c84535a360462d5
janmejoy <janmejoysarker@gmail.com>
2025-11-17 19:41:37 +0100

    RoI code is ready for non tracking RoI

--- src/suit_roi_align.py
+++ src/suit_roi_align.py
@@ -7,6 +7,10 @@ Mon Nov  3 05:34:31 PM CET 2025
 @hostname: machine
 
 DESCRIPTION
+- Remove contaminant spots from SUIT RoI images.
+- Uses cross correlation to align first 10 images of sequence.
+- Flat field is generated based on first frame of sequence.
+- Correction will only work as long as RoI position does not change due to tracking.
 """
 
 import matplotlib.pyplot as plt 
@@ -23,6 +27,11 @@ import numpy as np
 from astropy.io import fits
 
 def select_roi_with_mouse(sunpy_map, cmap=None, norm=None):
+    """
+    DESCRIPTION: To select RoI template.
+    INPUT: Sunpy map.
+    RETURNS: Sunpy submap. 
+    """
     fig = plt.figure()
     ax = fig.add_subplot(111, projection=sunpy_map)
     ax.set_title("Select ROI (click and drag) then close the window")
@@ -47,27 +56,28 @@ def select_roi_with_mouse(sunpy_map, cmap=None, norm=None):
     submap = sunpy_map.submap(bottom_left=bottom_left, top_right=top_right)
     return submap
 
-def align_maps(seq):
-    '''
-    Align a map sequence to the reference frame
-    
-    RETURNS:
-    Aligned sunpy map sequence
-    '''
+def align_maps(map_seq):
+    """
+    DESCRIPTION:
+    - Align a map sequence to the reference frame.
+    - First frame of a sequence is the reference frame.
+    INPUT:Unaligned map sequence.
+    RETURN: Aligned sunpy map sequence.
+   """
     # Ref submap is taken from the first frame of the sequence
-    ref_submap = select_roi_with_mouse(seq[0]) 
-    nt = len(seq)
+    ref_submap = select_roi_with_mouse(map_seq[0]) 
+    nt = len(map_seq)
     xshift_keep = np.zeros(nt) * u.pix
     yshift_keep = np.zeros_like(xshift_keep)
-    shifts = calculate_match_template_shift(seq, template=ref_submap,)
+    shifts = calculate_match_template_shift(map_seq, template=ref_submap)
     xshift_arcseconds = shifts["x"]
     yshift_arcseconds = shifts["y"]
-    for i, m in enumerate(seq):
+    for i, m in enumerate(map_seq):
         xshift_keep[i] = xshift_arcseconds[i] / m.scale[0]
         yshift_keep[i] = yshift_arcseconds[i] / m.scale[1]
-    seq = apply_shifts(seq, -yshift_keep, -xshift_keep, clip=True)
+    map_seq = apply_shifts(map_seq, -yshift_keep, -xshift_keep, clip=False)
     final_seq_2 = []
-    for i,j in enumerate(seq):
+    for i,j in enumerate(map_seq):
         date = j.date.strftime('%H:%M:%S')
         dhobt_dt = j.meta['dhobt_dt']
         grt_dt = j.meta['grt_dt']
@@ -79,7 +89,7 @@ def align_maps(seq):
         crtime = j.meta['crtime']
         exptime = j.meta['cmd_expt']
         meas_exptime = j.meta['meas_exp']
-        p = Map(j.data, seq[0].meta)
+        p = Map(j.data, map_seq[0].meta)
         p.meta['dhobt_dt'] = dhobt_dt
         p.meta['grt_dt'] = grt_dt
         p.meta['mfgdate'] = mfgdate
@@ -94,14 +104,16 @@ def align_maps(seq):
     final_seq_2 = Map(final_seq_2, sequence=True)
     return (final_seq_2)
 
-def generate_flat(final_seq_2, SAVE=False):
-    '''
-    Generate a flat field of contaminant spots.
-    RETURNS:
-    Flat field file. Give the option to save or not.
-    '''
-    ref_map=final_seq_2[0]
-    aligned_maps= [m.data for m in final_seq_2]
+def generate_flat(al_seq, SAVE=False):
+    """
+    DESCRIPTION:
+    - Generate a flat field of contaminant spots.
+    - Returns the flat field with the option to save or not.
+    INPUT: Aligned map sequence, option to SAVE (bool).
+    RETURNS: Flat field image array.
+    """
+    ref_map=al_seq[0]
+    aligned_maps= [m.data for m in al_seq]
 
     if (MODE=='median'):
         aligned_map_arr= np.stack(aligned_maps, axis=0)
@@ -117,41 +129,47 @@ def generate_flat(final_seq_2, SAVE=False):
         print('Flat file saved at', savepath)
     return flat
 
-if __name__=='__main__':
-    MODE='median'
-    project_path = os.path.abspath("..")
-    savepath= os.path.join(project_path, "data/interim/flat_frame.fits")
-    f_seq = sorted(glob.glob(os.path.join(project_path,'data/raw/*')))
-    seq = Map(f_seq, sequence=True)
-    aligned_sequence= align_maps(seq)
-    flat_frame= generate_flat(aligned_sequence, SAVE=True)
-    
-    ref_map= aligned_sequence[0]
-    corrected_map= Map(aligned_sequence[0].data/flat_frame, ref_map.meta)
-    corrected_map_ls= [Map(m.data/flat_frame, m.meta) for m in aligned_sequence]
-    corrected_map_seq= MapSequence(corrected_map_ls)
-    
-    '''
+def visualize(map1, flatframe, map3):
+    """
+    DESCRIPTION:
+    See a preview of the individual images.
+    INPUT:
+    - map1, map3: Sunpy Map
+    - flatframe: numpy 2D array
+    RETURNS: Visualization.
+    """
     VMN= 0
     VMX= np.max(ref_map.data)
     fig, ax= plt.subplots(1,3, sharex=True, sharey=True)
-    im0= ax[0].imshow(ref_map.data, origin='lower', vmin=VMN)
+    im0= ax[0].imshow(map1.data, origin='lower', vmin=VMN)
     ax[0].set_title('Raw')
     plt.colorbar(im0, ax=ax[0])
     
-    im1= ax[1].imshow(flat, origin='lower', vmin=VMN, vmax=1.2)
+    im1= ax[1].imshow(flatframe, origin='lower', vmin=VMN, vmax=1.2)
     ax[1].set_title('Calibration frame')
     plt.colorbar(im1, ax=ax[1])
     
-    im2= ax[2].imshow(corrected_img, origin='lower', vmin=VMN, vmax=VMX)
+    im2= ax[2].imshow(map3.data, origin='lower', vmin=VMN, vmax=VMX)
     ax[2].set_title('Corrected map')
     plt.colorbar(im2, ax=ax[2])
     plt.show()
 
-    #save_fits
-    save_path= os.path.join(project_path, 'data/processed/savefits.fits')
-    with fits.open(f_seq[0]) as hdu:
-        head= hdu[0].header
-    fits.writeto(save_path, corrected_img, header= head, overwrite= True)
-    print('File saved at', savepath)
-    '''
+if __name__=='__main__':
+    MODE='median' # options: 'median' or 'max'
+    PLOT= False # Plot preview?
+    SAVE=True # Save o/p fits?
+
+    project_path = os.path.abspath("..")
+    savepath= os.path.join(project_path, "data/interim/flat_frame.fits")
+    f_seq = sorted(glob.glob(os.path.join(project_path,'data/raw/*')))
+    seq = Map(f_seq, sequence=True)
+    aligned_sequence= align_maps(seq[:10]) # Generate flat using first 10 images of sequence
+    flat_frame= generate_flat(aligned_sequence, SAVE)
+    ref_map= aligned_sequence[0]
+    corrected_map_ls=[]
+    for m in seq: #Multiprocess to be implemented here
+        corrected_map= Map(m.data/flat_frame, m.meta)
+        img_savepath= os.path.join(project_path, 'data/processed/', m.meta['F_NAME'])
+        corrected_map.save(img_savepath, overwrite=True)
+        corrected_map_ls.append(corrected_map)
+    if PLOT: visualize(seq[0], flat_frame, corrected_map_ls[0])    
